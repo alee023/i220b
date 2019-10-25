3d43c6aa4380fb0c06869ff7b7b3146c21d44baf
Alison Lee <alee889@cs.binghamton.edu>
2019-10-24 22:28:17 -0400

    lab10 work

--- submit/lab10/files/parity/parity.s
+++ submit/lab10/files/parity/parity.s
@@ -4,5 +4,15 @@
 get_parity:
 
 	#@TODO: add code here to set eax to 1 iff edi has even parity
-	ret
+	testl %eax, %eax
+	jpe even
+	movl $0, %eax
+	jmp return_answer
+	
+even: 
+	movl $1, %eax
+	jmp return_answer
+
+return_answer: 
+	ret	
 	
